e9a8de8beadbb1cad82ff8ae00b3b4b3476ab0a2
vru6650 <vc5659076@gmail.com>
2025-08-28 09:35:07 +0530

    Improve slug generation and token verification

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
+
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return next(errorHandler(401, 'Unauthorized'));
-  }
-  jwt.verify(token, 'viren', (err, user) => {
-    if (err) {
-      return next(errorHandler(401, 'Unauthorized'));
+    const token = req.cookies?.access_token;
+    if (!token) {
+        return next(errorHandler(401, 'Unauthorized'));
     }
-    req.user = user;
-    next();
-  });
+
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+        return next(errorHandler(500, 'JWT secret is missing'));
+    }
+
+    try {
+        const user = jwt.verify(token, secret);
+        req.user = user;
+        next();
+    } catch {
+        next(errorHandler(401, 'Unauthorized'));
+    }
 };
